74e0eb8b8fd1a73591f9c7850375b66502eb9f57
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-02-29 22:16:03 +0900

    フロントのテスト用

--- index.py
+++ index.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route('/', methods=['POST'])
+def post():
+	key = request.form['apiKey']
+	text = request.form['setText']
+	voice = request.form['setVoice']
+	return render_template('index.html', getApiKey = key, getText = text, getVoice = voice)
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
