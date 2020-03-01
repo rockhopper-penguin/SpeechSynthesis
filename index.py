72a40899a44addf6f5e7a5c424ad3f9420a8611e
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-03-01 15:49:15 +0900

    音声合成API部分の実装

--- index.py
+++ index.py
@@ -1,4 +1,18 @@
 from flask import Flask, render_template, request
+import sys
+import requests
+
+adress = 'https://api.voicetext.jp/v1/tts'
+
+def getVoice(key, text, voice):
+	Parameters = {
+		'text': text,
+		'speaker': voice
+	}
+	send = requests.post(adress, params = Parameters, auth = (key,''))
+	result = open("result/result.wav", 'wb')
+	result.write(send.content)
+	result.close()
 
 app = Flask(__name__)
 
@@ -11,7 +25,8 @@ def post():
 	key = request.form['apiKey']
 	text = request.form['setText']
 	voice = request.form['setVoice']
-	return render_template('result.html', getApiKey = key, getText = text, getVoice = voice)
+	getVoice(key, text, voice)
+	return render_template('result.html')
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
